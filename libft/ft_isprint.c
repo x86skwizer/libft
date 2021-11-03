5c3142e1f7dd7fa2b4c29a70ee9e05c4846451db
Yassine Amrire <yamrire@e2r13p10.1337.ma>
2021-11-03 15:12:34 +0100

    libft

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,4 +1,18 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int main    
+int ft_isprint(char c)
+{
+    if (c >= 32 && c <= 127)
+        return (1);
+    else
+        return (0);
+}
+
+int main()
+{
+    char str = 'a';
+    printf("isprint    : |%d|\n", isprint(str));
+    printf("ft_isprint : |%d|\n", ft_isprint(str));
+    return (0);
+}
