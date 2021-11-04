da5c897f2766cf1ced38fc2fce0625f09ef23486
yamrire <yamrire@student.1337.ma>
2021-11-04 12:30:07 +0100

    ft_strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+
+ unsigned long ft_strlen(const char *str)
+ {
+	 unsigned long num;
+
+	 num = 0;
+	 while(str[num] != '\n')
+		 num++;
+	return (num);
+ }
+ 
+ int	main()
+{
+	char str[] = "Yassine";
+	printf("strlen    = |%lu|\n", strlen(str));
+	printf("ft_strlen = |%lu|\n", strlen(str));
+	return (0);
+}
