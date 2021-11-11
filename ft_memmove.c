5218f044485fc8e5076a75858ded5308335c6128
Yassine Amrire <yamrire@student.1337.ma>
2021-11-11 04:37:24 +0100

    resource

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,4 +6,5 @@ void *ft_memmove(void *dst, const void *src, size_t len)
     //https://stackoverflow.com/questions/1201319/what-is-the-difference-between-memmove-and-memcpy
     //https://riptutorial.com/c/example/2150/copying-overlapping-memory
     //https://www.youtube.com/channel/UC2hJBZwhrqtXkLVRwMXtujA
+    //https://cs50.stackexchange.com/questions/14615/memory-overlap-in-c
 }
