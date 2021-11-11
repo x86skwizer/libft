465bfba4541a9f4ec1869984689f06966e83baf3
Yassine Amrire <yamrire@e2r9p9.1337.ma>
2021-11-11 02:48:25 +0100

    libft

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,5 +3,7 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-    
+    //https://stackoverflow.com/questions/1201319/what-is-the-difference-between-memmove-and-memcpy
+    //https://riptutorial.com/c/example/2150/copying-overlapping-memory
+    //https://www.youtube.com/channel/UC2hJBZwhrqtXkLVRwMXtujA
 }
