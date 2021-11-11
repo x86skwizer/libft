5ce5f16e518b076d6d50dc45d6e4a917953459fb
Yassine Amrire <yamrire@e2r9p9.1337.ma>
2021-11-11 02:40:27 +0100

    libft

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -25,14 +25,20 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 
 int	main()
 {
-	unsigned char dest[20];
-	unsigned char dest1[20];
-	unsigned char src[20];
+	/*//unsigned char dest[20];
+	//unsigned char dest1[20];
+	unsigned char src[20] = "Start Stop";
 
-	memcpy(dest, src, 4);
-	ft_memcpy(dest1, src, 4);
-	printf("memcpy    : |%s|\n", dest);
-	printf("ft_memcpy : |%s|\n", dest1);
+	memcpy(src, src + 6, 4 * sizeof(unsigned char));
+	ft_memcpy(src, src + 6, 4 * sizeof(unsigned char));
+	printf("memcpy    : |%s|\n", src);
+	printf("ft_memcpy : |%s|\n", src);*/
+    char str[] = "foo-bar";
+    char str1[] = "foo-bar";
+    memmove(&str[3],&str[4],4);
+    ft_memcpy(&str1[3],&str1[4],4);
+    printf("memcpy    : |%s|\n", str);
+	printf("ft_memcpy : |%s|\n", str1);
 	return 0;
 
 }
