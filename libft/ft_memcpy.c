ffc738e21d08d34476aa1dc237d03f14659633d3
Yassine Amrire <yamrire@e2r13p10.1337.ma>
2021-11-12 01:05:58 +0100

    libft

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -10,37 +10,35 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
     dest = (unsigned char *) dst;
     sorc = (unsigned char *) src;
 
+    if (!dst || !src) return NULL;
+    
     i = 0;
-    if (sorc && i < n)
+    while (i < n)
     {
-        while (sorc[i] != '\0' && i < n)
-        {
-            dest[i] = sorc[i];
-            i++;
-        }
+        dest[i] = sorc[i];
+        i++;
     }
-
     return (dest);
 }
 
 int	main()
 {
-	/*//unsigned char dest[20];
-	//unsigned char dest1[20];
-	unsigned char src[20] = "Start Stop";
+	// //unsigned char dest[20];
+	// //unsigned char dest1[20];
+	// unsigned char src[20] = "Start Stop";
 
-	memcpy(src, src + 6, 4 * sizeof(unsigned char));
-	ft_memcpy(src, src + 6, 4 * sizeof(unsigned char));
-	printf("memcpy    : |%s|\n", src);
-	printf("ft_memcpy : |%s|\n", src);
-    char str[] = "foo-bar";
-    char str1[] = "foo-bar";
-    memmove(&str[3],&str[4],4);
-    ft_memcpy(&str1[3],&str1[4],4);
-    printf("memcpy    : |%s|\n", str);
-	printf("ft_memcpy : |%s|\n", str1);*/
+	// memcpy(src, src + 6, 4 * sizeof(unsigned char));
+	// ft_memcpy(src, src + 6, 4 * sizeof(unsigned char));
+	// printf("memcpy    : |%s|\n", src);
+	// printf("ft_memcpy : |%s|\n", src);
+    // char str[] = "foo-bar";
+    // char str1[] = "foo-bar";
+    // memmove(&str[3],&str[4],4);
+    // ft_memcpy(&str1[3],&str1[4],4);
+    // printf("memcpy    : |%s|\n", str);
+	// printf("ft_memcpy : |%s|\n", str1);
     char str1[] = "abcde";
-	ft_memcpy(str1 + 2, str1, 3);
+	memcpy(str1 + 2, str1, 3);
     printf("memcpy  : |%s|\n", str1);
 	return 0;
 
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+void *ft_memcpy(void *dst, const void *src, size_t n);
+
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*dest;
-	unsigned char	*sorc;
-
-	dest = (unsigned char *) dest;
-	sorc = (unsigned char *) sorc;
-
-	if
+	if (!dst || !src)
+		return NULL;
+	while (len--)
+		((char *)dst)[len] = ((char *)src)[len];
+	return (dst);
 }
 
 int	main()
 {
 	char str[] = "abcde";
 	char str1[] = "abcde";
+	//char str2[] = "abcde";
+	ft_memmove(str1 + 2, str1, 3);
+	//memcpy(str1 + 2, str1, 3);
 	memmove(str + 2, str, 3);
-	memcpy(str1 + 2, str1, 3);
-	printf("memmove : |%s|\n", str);
-	printf("memcpy  : |%s|\n", str1);
+	//printf("memcpy     : |%s|\n", str1);
+	printf("memmove    : |%s|\n", str);
+	printf("ft_memmove : |%s|\n", str1);
 	return 0;
 }
