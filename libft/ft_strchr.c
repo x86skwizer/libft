10b6869a2cb86f43e0758731a92f859daf81b74a
Yassine Amrire <yamrire@e2r4p7.1337.ma>
2021-11-14 01:02:51 +0100

    libft

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-char    *ft_strchr(const char *s, int c)
+ char    *ft_strchr(const char *str, int c)
 {
-    
+    char alpha;
+    int num;
+    char *s = (char *)str;
+    alpha = (char) c;
+    if (!s)
+        return (0);
+    num = 0;
+    while (s[num] != '\0')
+    {
+        if (s[num] == alpha)
+            return(&s[num]);
+        num++;
+    }
+    if (c == '\0')
+        return("\0");
+    return (NULL);
+}
+
+int main()
+{
+    printf("%s\n", ft_strchr("Yassine", 'e'));
+    printf("%s\n",strchr("Yassine", 'e'));
+    return (0);
 }
