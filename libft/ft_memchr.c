bceb6f78a98a7614f67085e5523cb249a0217bb5
Yassine Amrire <yamrire@e2r4p7.1337.ma>
2021-11-14 04:33:53 +0100

    libft

---
+++ libft/ft_memchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+    char    *str;
+    unsigned char alpha;
+    size_t num;
+
+    if (n == 0)
+        return (NULL);
+    str = (char *)s;
+    alpha = (unsigned char) c;
+    num = 0;
+    while (str[num] != '\0')
+    {
+        if (str[num] == alpha)
+           return(&str[num]);
+        num++;
+    }
+    return(NULL);
+}
+
+int main()
+{
+    
+    printf("%s\n", ft_memchr("yassine", 's', 6));
+    return 0;
+}
