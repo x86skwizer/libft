7892bfc9a921551b3f76fc80025a107cf6f03f86
Yassine Amrire <yamrire@e2r11p9.1337.ma>
2021-11-19 23:46:34 +0100

    libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t ft_strlen(const char *str);
+
+char    *ft_strdup(const char *src)
+{
+	char	*ptr;
+	unsigned int	num;
+
+	num = 0;
+	ptr = (char *) malloc(ft_strlen(src) * sizeof(char));
+	while (src[num] != '\0')
+	{
+		ptr[num] = src[num];
+		num++;
+	}
+	return (ptr);
+}
+
+int	main()
+{
+	char *str = "yassine";
+	char *str1;
+	str1 = ft_strdup(str);
+	printf("%s\n", str1);
+	return 0;
+}
