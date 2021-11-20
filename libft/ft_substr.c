0b199ac3da6d5d5381dc7271814d4f7c348fbf3d
Yassine Amrire <yamrire@e2r5p5.1337.ma>
2021-11-20 22:26:31 +0100

    libft2

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t ft_strlen(const char *str);
+
 char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
     char    *str;
-    unsigned int    num;
-
-    str = (char *) malloc((len + 1) * sizeof(char));
+    size_t    num;
+    
+    
+    if (len > ft_strlen(s))
+    {
+        len = ft_strlen(s);
+        str = (char *) malloc((len + 1) * sizeof(char));
+    }
+    else
+        str = (char *) malloc((len + 1) * sizeof(char));
     if (!str)
         return (NULL);
     num = 0;
-    while (start < start + len)
+    while (start + num < len)
     {
-        str[num] = s[start];
+        str[num] = s[start + num];
         num++;
-        start++;
     }
     str[num] = '\0';
     return (str);
@@ -23,6 +31,6 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
 int main()
 {
     char str[15] = "Yassine Amrire";
-    printf("%s\n", ft_substr(str, 3, 5));
+    printf("%s\n", ft_substr(str, 3, 11));
     return (0);
 }
