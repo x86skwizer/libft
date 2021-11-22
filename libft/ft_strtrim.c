93e80084b04f13da78f491bcf0b8da39d70cd0b8
Yassine Amrire <yamrire@e2r13p11.1337.ma>
2021-11-22 04:18:12 +0100

    libft2

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -15,15 +15,14 @@ char    *ft_strtrim(char const *s1, char const *set)
 	while (ft_strchr(set, s1[i]))
 		i++;
 	j = ft_strlen(s1) - 1;
-	while (ft_strchr(set, s1[j]))
+	while (j >= 0 && ft_strchr(set, s1[j]))
 		j--;
 	return (ft_substr(s1, i, j - i));
 }
 
 int	main()
 {
-	char str[] = "123123333222111";
+	char str[] = "11111111";
 	printf("%s\n", ft_strtrim(str, "123"));
-	printf("%lu\n", sizeof(size_t));
 	return (0);
 }
