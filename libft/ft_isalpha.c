65bf62aa727379de070726a76cd765b965b292a6
Yassine Amrire <yamrire@e2r13p3.1337.ma>
2021-12-10 00:27:29 +0100

    libftf

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -14,20 +14,8 @@
 
 int	ft_isalpha(int c)
 {
-	unsigned char	a;
-
-	a = (unsigned char)c;
-	if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
 }
-
-/*int main()
-{
-    char str = 'a';
-    printf("isalpha    : |%d|\n", isalpha(str));
-    printf("ft_isalpha : |%d|\n", ft_isalpha(str));
-    return (0);
-
-}*/
