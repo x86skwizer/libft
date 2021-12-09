f2c8f34ca70e746e504a1cad21cd347869ddab92
Yassine Amrire <yamrire@e2r13p3.1337.ma>
2021-12-10 00:13:02 +0100

    libftf

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <stdio.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -23,21 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	len = 0;
-	while (str1[len] == str2[len] && len < n)
-			len++;
-	len--;
-	return (str1[len] - str2[len]);
-}
-
-
-int	main ()
-{
-	char *s1 = "\xff\xaa\xde\x12WXYZ";
-	char *s2 = "\xff\xaa\xde\x12MACOSX";
-	size_t size = 7;
-	int i1 = (memcmp(s1, s2, size));
-	int i2 = (ft_memcmp(s1, s2, size));
-	printf("memcmp : %d\n", i1);
-	printf("ft_memcmp : %d\n", i2);
-
+	while (len < n)
+	{
+		if (str1[len] != str2[len])
+			return (str1[len] - str2[len]);
+		len++;
+	}
+	return (0);
 }
