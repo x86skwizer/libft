c38c8bdeaf3c6c8a57c3d1118ba215a8bed1f7b3
Yassine Amrire <yamrire@student.1337.ma>
2021-12-10 01:51:46 +0100

    libftf

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -10,23 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 int	ft_isprint(int c)
 {
-	unsigned char	a;
-
-	a = (unsigned char)c;
-	if (a >= 32 && a <= 127)
+	if (c >= 0040 && c <= 0176)
 		return (1);
 	else
 		return (0);
 }
-
-/*int main()
-{
-    int str = ' ';
-    printf("isprint    : |%d|\n", isprint(str));
-    printf("ft_isprint : |%d|\n", ft_isprint(str));
-    return (0);
-}*/
