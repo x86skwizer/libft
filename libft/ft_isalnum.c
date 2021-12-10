384b83ef923ef2a6e06a0c5abc546d2077a7a6d7
Yassine Amrire <yamrire@student.1337.ma>
2021-12-10 01:53:09 +0100

    libftf

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -10,23 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 int	ft_isalnum(int c)
 {
-	unsigned char	a;
-
-	a = (unsigned char) c;
-	if ((a >= 48 && a <= 57) || (a >= 65 && a <= 90) || (a >= 97 && a <= 122))
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
 }
-
-/*int main()
-{
-    int str = 97;
-    printf("isalnum    : |%d|\n", isalnum(str));
-    printf("ft_isalnum : |%d|\n", ft_isalnum(str));
-    return (0);
-}*/
