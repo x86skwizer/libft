3a5bd261d5fbb69027dbb5c70feb09b1adced45c
Yassine Amrire <yamrire@student.1337.ma>
2021-12-13 16:19:14 +0100

    libftf

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -19,11 +19,3 @@ int	ft_isascii(int c)
 	else
 		return (0);
 }
-
-/*int main()
-{
-    char str = '}';
-    printf("isascii    : |%d|\n", isascii(str));
-    printf("ft_isascii : |%d|\n", ft_isascii(str));
-    return (0);
-}*/
