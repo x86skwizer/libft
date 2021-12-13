a7b693d01d7209a95a38d37ef776d32fbcbcbdb0
Yassine Amrire <yamrire@student.1337.ma>
2021-12-13 16:16:33 +0100

    libftf

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -16,29 +16,3 @@ void	ft_bzero(void *s, size_t len)
 {
 	ft_memset(s, 0, len);
 }
-
-/*int	main()
-{
-	char str[] = "Yassine";
-
-	int	intd;
-	intd = 0;
-	// while (intd < 4)
-	// {
-	// 	printf(":>%s\n", arr[intd]);
-	// 	intd++;
-	// }
-
-	ft_bzero(str, 4 * sizeof(char));
-	intd = 0;
-	printf("test :>%s\n", str);
-	printf("test :>%s\n", str + 4);
-
-	// while (intd < 4)
-	// {
-	// 	printf("test :>%d\n", arr[intd]);
-	// 	intd++;
-	// }
-	
-	return (0);
-}*/
