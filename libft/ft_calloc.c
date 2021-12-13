591ef18e40b608ae8a31447334b5947850a36cab
Yassine Amrire <yamrire@student.1337.ma>
2021-12-13 16:18:09 +0100

    libftf

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -22,20 +22,3 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_memset(ptr, 0, size * count);
 	return (ptr);
 }
-
-/*int    main()
-{
-    char *str;
-	int len;
-    len  = 5;
-    str = (char *) ft_calloc(len, sizeof(char));
-    int i = -1 ;
-    while (++i < len)
-        str[i] = 'a';
-    i = -1;
-    printf("Before the loop\n");
-    while (++i < len)
-        printf("[%d]: %c\n", i, str[i]);
-    return (0);
-
-}*/
