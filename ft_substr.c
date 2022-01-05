59f98175c4a17572c634849e46e958d6a4670857
Yassine Amrire <yamrire@student.1337.ma>
2022-01-05 19:17:18 +0100

    Updating ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 02:43:37 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 17:09:29 by yamrire          ###   ########.fr       */
