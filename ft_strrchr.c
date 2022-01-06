8a8f6fe34a1b27f083b4fd99c27c1a943d13acaf
Yassine Amrire <yamrire@student.1337.ma>
2022-01-06 10:05:46 +0100

    Updating ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 02:39:19 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/10 05:05:47 by yamrire          ###   ########.fr       */
