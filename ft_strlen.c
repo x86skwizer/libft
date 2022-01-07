d7cdfea80fc74f299ace4feff68796a8ca0207f8
Yassine Amrire <yamrire@student.1337.ma>
2022-01-07 10:15:27 +0100

    Updating ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 02:23:44 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 16:58:38 by yamrire          ###   ########.fr       */
