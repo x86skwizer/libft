0c648cdd388428c4eb0298651ea53d569c478567
Yassine Amrire <yamrire@student.1337.ma>
2022-01-09 13:42:36 +0100

    Updating ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 00:12:27 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 16:53:17 by yamrire          ###   ########.fr       */
