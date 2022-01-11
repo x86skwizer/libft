5f8c138d77884deb38f1f0bd0d9120921ab8fbb9
Yassine Amrire <yamrire@student.1337.ma>
2022-01-11 13:54:20 +0100

    Updating ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:00:47 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 14:32:59 by yamrire          ###   ########.fr       */
