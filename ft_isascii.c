d5bd2b6a693fb0cfda5e04434771b78657de5e8d
Yassine Amrire <yamrire@student.1337.ma>
2022-01-13 00:40:12 +0100

    Updating ft_isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 22:24:57 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 14:23:37 by yamrire          ###   ########.fr       */
