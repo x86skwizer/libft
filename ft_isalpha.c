ee82f3e2974cbe94d97471b8fb8380824001bb1b
Yassine Amrire <yamrire@student.1337.ma>
2022-01-13 00:41:41 +0100

    Updating ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 22:21:38 by yamrire           #+#    #+#             */
 /*   Updated: 2021/12/07 14:23:17 by yamrire          ###   ########.fr       */
