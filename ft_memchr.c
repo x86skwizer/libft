b38885b4c6fc21239e8ed9960168b2b3db5fac6b
Yassine Amrire <yamrire@student.1337.ma>
2022-03-16 01:08:36 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
