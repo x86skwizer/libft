97cd3f236c31d632ade354fa7e44320b99064cff
Yassine Amrire <yamrire@student.1337.ma>
2022-03-16 01:14:31 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
